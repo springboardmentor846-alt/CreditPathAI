a034e4368eab6b1c71bed421ec411818dc12ae73
Divyashree N <divyashreen2611@gmail.com>
2025-12-10 17:31:56 +0000

    Progress Updated

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 const API_URL = "https://orange-fortnight-69ppvpqgpx66hrvpv-8000.app.github.dev";
 
 export async function predictRisk(inputData) {
-  const response = await fetch(`${API_URL}/predict/`, {  // <-- FIXED
+  const response = await fetch(`${API_URL}/predict/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(inputData),
